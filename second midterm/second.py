1d48992140776bb38db7dbe110753a1271bcfe91
Filip <filiptrajkoski2005@gmail.com>
2026-05-25 18:04:19 +0200

    Refactor dataset processing and model evaluation
    Refactored the code to streamline dataset handling and model evaluation. Added accuracy scoring for predictions.

--- second midterm/second.py
+++ second midterm/second.py
@@ -1,38 +1,33 @@
 import os
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-
 import warnings
 warnings.filterwarnings("ignore")
 
 from data.dataFirst import dataset
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
 if __name__ == '__main__':
     C = int(input())
     N = int(input())
     S = int(input())
 
-    original_data = []
+    new_dataset = []
     for row in dataset:
         new_row = row[:-1]
         if row[-1] >= 50:
             new_row.append(1)
         else:
             new_row.append(0)
-        original_data.append(new_row)
+        new_dataset.append(new_row)
 
-    split = int(len(original_data) * 0.7)
+    split = int(len(new_dataset) * 0.7)
 
-    train_X_org = [row[:-1] for row in original_data[:split]]
-    train_Y_org = [row[-1] for row in original_data[:split]]
+    removed_anomaly = []
 
-    test_X_org = [row[:-1] for row in original_data[split:]]
-    test_Y_org = [row[-1] for row in original_data[split:]]
-
-    removed_anomalies = []
-    for row in original_data:
+    for row in new_dataset:
         new_row = row[:]
         if row[3] > C:
             new_row[3] = C
@@ -40,58 +35,34 @@ if __name__ == '__main__':
             new_row[4] = N
         if row[5] > S:
             new_row[5] = S
-        removed_anomalies.append(new_row)
+        removed_anomaly.append(new_row)
 
-    train_X_rm = [row[:-1] for row in removed_anomalies[:split]]
-    train_Y_rm = [row[-1] for row in removed_anomalies[:split]]
+    model = MLPClassifier (hidden_layer_sizes=(50,),activation='relu',learning_rate_init=0.001,max_iter=25)
 
-    test_X_rm = [row[:-1] for row in removed_anomalies[split:]]
-    test_Y_rm = [row[-1] for row in removed_anomalies[split:]]
-
-    # scaled
-
-    train_X_sc = [row[:-1] for row in original_data[:split]]
-    train_Y_sc = [row[-1] for row in original_data[:split]]
-
-    test_X_sc = [row[:-1] for row in original_data[split:]]
-    test_Y_sc = [row[-1] for row in original_data[split:]]
-
-    scaler = StandardScaler()
-    scaler.fit(train_X_sc)
-    train_X_sc = scaler.transform(train_X_org)
-    test_X_sc = scaler.transform(test_X_org)
-
-    train_X_rm_sc = [row[:-1] for row in removed_anomalies[:split]]
-    train_Y_rm_sc = [row[-1] for row in removed_anomalies[:split]]
-
-    test_X_rm_sc = [row[:-1] for row in removed_anomalies[split:]]
-    test_Y_rm_sc = [row[-1] for row in removed_anomalies[split:]]
-
-    scaler = StandardScaler()
-    scaler.fit(train_X_rm_sc)
-    train_X_rm_sc = scaler.transform(train_X_rm_sc)
-    test_X_rm_sc = scaler.transform(test_X_rm_sc)
-
-    dictionary_something = {0: [train_X_org, train_Y_org, test_X_org, test_Y_org],
-                            1: [train_X_rm, train_Y_rm, test_X_rm, test_Y_rm],
-                            2: [train_X_sc, train_Y_sc, test_X_sc, test_Y_sc],
-                            3: [train_X_rm_sc, train_Y_rm_sc, test_X_rm_sc, test_Y_rm_sc]}
-
-    accuracies = []
-
+    datasets = [new_dataset,removed_anomaly,new_dataset,removed_anomaly]
+    accuracy = []
     for i in range(4):
-        model = MLPClassifier(hidden_layer_sizes=(50,), activation='relu', learning_rate_init=0.001,
-                              max_iter=25, random_state=0)
-        train_X = dictionary_something[i][0]
-        train_Y = dictionary_something[i][1]
-        test_X = dictionary_something[i][2]
-        test_Y = dictionary_something[i][3]
+        dataset = datasets[i]
+        train_X = [row[:-1] for row in dataset[:split]]
+        train_Y = [row[-1] for row in dataset[:split]]
+        
+        test_X = [row[:-1] for row in dataset[split:]]
+        test_Y = [row[-1] for row in dataset[split:]]
+
+        if i == 2 or i == 3:
+            scaler = StandardScaler()
+            scaler.fit(train_X)
+            train_X = scaler.transform(train_X)
+            test_X = scaler.transform(test_X)
+
+        model = MLPClassifier(hidden_layer_sizes=(50,), activation='relu', learning_rate_init=0.001, max_iter=25, random_state=0)
+
         model.fit(train_X, train_Y)
-        prediction = model.predict(test_X)
-        accuracies.append(sum(1 for v, t in zip(prediction, test_Y) if v == t) / len(test_Y))
+        predictions = model.predict(test_X)
+        accuracy.append(accuracy_score(test_Y, predictions))
 
     print("Accuracy with:")
-    print(f'The original dataset: {accuracies[0]}')
-    print(f'Removed anomalies: {accuracies[1]}')
-    print(f'Scaled attributes: {accuracies[2]}')
-    print(f'Removed anomalies and scaled attributes: {accuracies[3]}')
+    print(f'The original dataset: {accuracy[0]}')
+    print(f'Removed anomalies: {accuracy[1]}')
+    print(f'Scaled attributes: {accuracy[2]}')
+    print(f'Removed anomalies and scaled attributes: {accuracy[3]}')
